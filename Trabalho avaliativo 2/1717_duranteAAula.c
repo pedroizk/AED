2815d93717f64ccc2fa6583be093822aa77dded6
Pedro Izkovitz <pedroizk@gmail.com>
2025-06-28 12:38:01 -0300

    finalmente deu certo

---
+++ Trabalho avaliativo 2/1717_duranteAAula.c
@@ -0,0 +1,124 @@
+
+typedef struct {
+int topo;
+char* pilha;
+} Stack;
+
+Stack* inicializar();
+char pop(Stack* pilha);
+void push(Stack* pilha, char valor);
+
+int maximumGain(char* s, int x, int y) {
+
+Stack* p = inicializar();
+char* tempString = malloc(sizeof(char) * 4 );
+char valor;
+int pontos = 0;
+
+for(int i = 0; i < strlen(s); i++)
+{
+push(p, s[i]);
+}
+
+while(p->pilha != NULL)
+{
+for(int i = 0; i < 3; i++)
+{
+tempString[i] = pop(p);
+}
+tempString[3] = '\0';
+
+int v1= tempString[0];
+int v2 = tempString[1];
+int v3 = tempString[2];
+
+if(x > y) // se ab der mais pontos que ba ---- bab abb baa aba aab
+{
+// Valores estao invertidos logo meu ab é ba
+if((v1 == 'b' && v2 == 'a'))
+{
+push(p, v3);
+pontos += 5;
+break;
+}
+else if(v3 == 'a' && v2 == 'b')
+{
+push(p, v1);
+pontos += 5;
+
+}
+else if(v3 == 'b' && v2 == 'a')
+{
+push(p, v1);
+pontos += 4;
+
+}
+else if(v2 == 'b' && v1 == 'a')
+{
+push(p, v1);
+pontos += 4;
+
+}
+
+else {
+push(p,v1);
+push(p, v2);
+push(p, v3);
+}
+
+
+}
+
+else if(y < x)
+{
+
+}
+
+else {
+
+}
+
+
+
+}
+return pontos;
+}
+
+// Ou, empilho tudo, quando terminar de empilhar, vou tirando de 3 em 3
+// cdbcbbaaabab -> bab -> se eu escolho um deles o outro vai de volta para a pilha
+// cdbcbbaaa -> bab -> cdbcbbaaab -> aab -> ab -> cdbcbbaa -> aab
+
+
+Stack* inicializar() {
+
+Stack* nova_pilha = malloc(sizeof(Stack));
+nova_pilha->topo = 0;
+nova_pilha->pilha = malloc(sizeof(char));
+return nova_pilha;
+
+}
+
+
+void push(Stack* pilha, char valor) {
+
+pilha->pilha = realloc(pilha->pilha, sizeof(char) * ( pilha->topo + 1));
+pilha->pilha[pilha->topo] = valor;
+pilha->topo += 1;
+
+}
+
+
+char pop(Stack* pilha) {
+
+if(pilha->topo == 0)
+return '\0';
+
+char valor_a_retornar = pilha->pilha[pilha->topo-1];
+
+pilha->topo -=1;
+pilha->pilha = realloc(pilha->pilha, sizeof(char) * ( pilha->topo));
+
+
+return valor_a_retornar;
+
+}
---
+++ Trabalho avaliativo 2/1717_versaoFinal.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int topo;
+    char* pilha;
+    int capacidade;
+} Stack;
+
+Stack* inicializar(int capacidade);
+char pop(Stack* pilha);
+void push(Stack* pilha, char valor);
+int counterAB(char* s, int x, Stack* p);
+int counterBA(char* s, int x, Stack* p);
+
+int maximumGain(char* s, int x, int y) {
+
+    int capacidade = strlen(s);
+    Stack* p = inicializar(capacidade);
+    Stack* p2 = inicializar(capacidade);
+    char* aux1 = s;
+    char* aux2 = NULL;
+    
+
+    if (strlen(s) > 1)
+        aux2 = s + 1;
+
+    int t = strlen(s);
+    char valor;
+    int pontos = 0;
+    char* stringAux = malloc(strlen(s) + 1);
+
+    if (x > y) {
+        pontos += counterAB(s, x, p);
+        int j = 0;
+        while (p->topo != 0) {
+            stringAux[j] = pop(p);
+            j++;
+        }
+        stringAux[j] = '\0';
+        printf("%s", stringAux);
+        pontos += counterAB(stringAux, y, p2);
+    }
+
+    else {
+        pontos += counterBA(s, y, p);
+        int j = 0;
+        while (p->topo != 0) {
+            stringAux[j] = pop(p);
+            j++;
+        }
+        stringAux[j] = '\0';
+        printf("%s", stringAux);
+        pontos += counterBA(stringAux, x, p2);
+    }
+
+    return pontos;
+}
+
+int counterAB(char* s, int x, Stack* p) {
+    int pontos = 0;
+
+
+    int t = strlen(s);
+
+    for (int i = 0; i < (t); i++) {
+
+        if (i + 1 < t && s[i] == 'a' && s[i + 1] == 'b') {
+            i++;
+            pontos += x;
+
+        }
+        
+         else if (s[i] == 'b' && p->topo > 0 &&
+                   p->pilha[p->topo - 1] == 'a') {
+            pop(p);
+            pontos += x;
+        } 
+        
+        else {
+            push(p, s[i]);
+        }
+
+    }
+
+    return pontos;
+}
+
+int counterBA(char* s, int x, Stack* p) {
+    int pontos = 0;
+    // qaawbbbwc
+
+    int t = strlen(s);
+
+
+    for (int i = 0; i < (t); i++) {
+
+        if (i + 1 < t && s[i] == 'b' && s[i + 1] == 'a') {
+            i++;
+            pontos += x;
+
+        } else if (s[i] == 'a' && p->topo > 0 &&
+                   p->pilha[p->topo - 1] == 'b') {
+            pop(p);
+            pontos += x;
+        }  else {
+            push(p, s[i]);
+        }
+
+    }
+
+    return pontos;
+}
+
+Stack* inicializar(int capacidade) {
+    Stack* nova_pilha = malloc(sizeof(Stack));
+    nova_pilha->topo = 0;
+    nova_pilha->pilha = malloc(sizeof(char) * capacidade);
+    return nova_pilha;
+}
+
+void push(Stack* pilha, char valor) {
+    pilha->pilha[pilha->topo] = valor;
+    pilha->topo += 1;
+}
+
+char pop(Stack* pilha) {
+    if (pilha->topo == 0)
+        return '\0';
+
+    char valor_a_retornar = pilha->pilha[pilha->topo - 1];
+    pilha->topo -= 1;
+    return valor_a_retornar;
+}
